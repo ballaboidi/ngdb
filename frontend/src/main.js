9a935439521ed3bbbffcc9c8f018258eb6422db2
Mario Autherith <mario.ta@gmx.at>
2020-05-10 21:48:58 +0200

    first Init

---
+++ frontend/src/main.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import VueApollo from "vue-apollo"
+import apolloClient from "./vue-apollo"
+
+Vue.config.productionTip = false
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({  
+  defaultClient: apolloClient
+})
+
+new Vue({
+  apolloProvider,
+  render: h => h(App),
+}).$mount('#app')
